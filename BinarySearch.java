1326a65a262bdb16e6f91dce9929a291d2974e9c
kumarsanjeetmishra8000@gmail.com <kumarsanjeetmishra8000@gmail.com>
2024-04-01 23:01:28 +0530

    Master:change-the-searching.

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,29 +1,31 @@
-import java.time.Clock;
 import java.util.*;
-public class BinarySearch {
-
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
+public class LinearSearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("please enter the size of an array");
-        int n=sc.nextInt();
+        int n = sc.nextInt();
         int[] arr = new int[n];
 
         System.out.println("please enter the data in array");
-        for(int i=0; i<arr.length; i++){
-            arr[i]=sc.nextInt();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
         }
-
-        System.out.println("enter the search data:");
+        System.out.println("please enter the data for searching.");
         int data=sc.nextInt();
-        boolean isData=false;
-        for(int i=0; i<n; i++){
-            if(arr[i]==data){
-                System.out.println("data fount at index"+i);
-                isData=true;
+        int start=0;
+        int end=arr[n-1];
+
+        while(start<=end){
+            int mid=(start+end)/2;
+
+            if(data==arr[mid]){
+                System.out.print("data found at index"+mid);
+                return;
+            }if(data<arr[mid]){
+                end=mid-1;
+            }if(data>arr[mid]){
+                start=mid+1;
             }
-        }
-        if(!isData){
-            System.out.println("data not found in this array.");
         }
     }
 }
--- LinearSearch.java
+++ LinearSearch.java
@@ -1,31 +1,29 @@
+import java.time.Clock;
 import java.util.*;
-public class LinearSearch {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+public class BinarySearch {
+
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
         System.out.println("please enter the size of an array");
-        int n = sc.nextInt();
+        int n=sc.nextInt();
         int[] arr = new int[n];
 
         System.out.println("please enter the data in array");
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = sc.nextInt();
+        for(int i=0; i<arr.length; i++){
+            arr[i]=sc.nextInt();
         }
-        System.out.println("please enter the data for searching.");
+
+        System.out.println("enter the search data:");
         int data=sc.nextInt();
-        int start=0;
-        int end=arr[n-1];
-
-        while(start<=end){
-            int mid=(start+end)/2;
-
-            if(data==arr[mid]){
-                System.out.print("data found at index"+mid);
-                return;
-            }if(data<arr[mid]){
-                end=mid-1;
-            }if(data>arr[mid]){
-                start=mid+1;
+        boolean isData=false;
+        for(int i=0; i<n; i++){
+            if(arr[i]==data){
+                System.out.println("data fount at index"+i);
+                isData=true;
             }
+        }
+        if(!isData){
+            System.out.println("data not found in this array.");
         }
     }
 }
